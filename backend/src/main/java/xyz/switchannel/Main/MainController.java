dc6fe90ad4b7c2b2f2431c9143b78aab955c358d
Radim Staněk <stanek.radim@gtomy.eu>
2022-03-01 13:19:22 +0100

    Initialize controller

---
+++ backend/src/main/java/xyz/switchannel/Main/MainController.java
@@ -0,0 +1,18 @@
+package xyz.switchannel.Main;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+
+    @GetMapping("/")
+    public String getTest() {
+        return "Test";
+    }
+
+    @GetMapping("/test")
+    public String getTest2() {
+        return "Test 2";
+    }
+}
