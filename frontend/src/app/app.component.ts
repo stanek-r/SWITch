6ff88a08b00b95b2f391760f2fd99e7d44ee42ca
Radim Staněk <stanek.radim@gtomy.eu>
2022-03-13 19:32:29 +0100

    Format and add videojs

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+declare var videojs: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'swit-ch';
+export class AppComponent implements OnInit {
+  public streamUrl?: string;
+
+  ngOnInit(): void {
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    if (urlParams.has('streamKey')) {
+      this.streamUrl =
+        'https://stream.swit-ch.live/source/' +
+        urlParams.get('streamKey') +
+        '/index.m3u8';
+    }
+    videojs('streamPlayer').play();
+  }
 }
